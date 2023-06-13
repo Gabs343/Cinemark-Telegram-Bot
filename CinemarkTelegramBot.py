fe5c7b171abb6825a3cd79b6663cade721ec83b0
Gabs343 <gabriellopezp343@gmail.com>
2023-06-13 16:02:27 -0300

    arreglando diccionarios y funciones

--- CinemarkTelegramBot.py
+++ CinemarkTelegramBot.py
@@ -24,20 +24,14 @@ async def today_functions_command(update: Update, context: ContextTypes.DEFAULT_
         else:
             
             for functionDate in functionsDict:
-                functionsMessage += "*bold****{:0}***\n".format(functionDate)
+                functionsMessage += "***{:0}***\n".format(functionDate)
                 
-                for functionFormat, functionHours in functionsDict.items():
-                    functionsMessage += "*bold*{:0}\n".format(functionFormat)
+                for functionFormat, functionHours in functionsDict[functionDate].items():
+                    functionsMessage += "{:0}\n".format(functionFormat)
                     
                     for functionHour in functionHours:
                         functionsMessage += "*{:0}\n".format(functionHour)
-            
-            """
-            for functionFormat, functionHours in functionsDict.items():
-                functionsMessage += "*bold*{:0}\n".format(functionFormat)
-                for functionHour in functionHours:
-                    functionsMessage += "*{:0}\n".format(functionHour)
-            """
+                        
     else:
        functionsMessage = "You are missing some parameters" 
 
@@ -48,26 +42,20 @@ async def functions_date_command(update: Update, context: ContextTypes.DEFAULT_T
     functionsMessage = ""
     if(len(context.args) == 3):
         
-        functionsDict = cinemark.get_today_functions(context.args[0], context.args[1], context.args[2])
+        functionsDict = cinemark.get_functions_for_date(context.args[0], context.args[1], context.args[2])
         if(isinstance(functionsDict, str)):
             functionsMessage = functionsDict
         else:
             
             for functionDate in functionsDict:
-                functionsMessage += "*bold****{:0}***\n".format(functionDate)
+                functionsMessage += "***{:0}***\n".format(functionDate)
                 
-                for functionFormat, functionHours in functionsDict.items():
-                    functionsMessage += "*bold*{:0}\n".format(functionFormat)
+                for functionFormat, functionHours in functionsDict[functionDate].items():
+                    functionsMessage += "{:0}\n".format(functionFormat)
                     
                     for functionHour in functionHours:
                         functionsMessage += "*{:0}\n".format(functionHour)
-            
-            """
-            for functionFormat, functionHours in functionsDict.items():
-                functionsMessage += "*bold*{:0}\n".format(functionFormat)
-                for functionHour in functionHours:
-                    functionsMessage += "*{:0}\n".format(functionHour)
-            """
+
     else:
        functionsMessage = "You are missing some parameters" 
 
@@ -136,7 +124,7 @@ async def dates_movie_command(update: Update, context: ContextTypes.DEFAULT_TYPE
     await update.message.reply_text(datesMessage)
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    helpMessage = "This bot only works with commands:\n\n1. /getmovies brings a list of the current movies in cinemark.\n\n2. /getcinemas brings a list of the current cinemas in cinemark.\n\n3. /getcinemasformovie brings a list of the current cinemas that display the movie that you're looking for. Use one parameter: movie.\n\n4. /getmoviesforcinema brings a list of the current movies that are in the cinema that you're looking for. Use one parameter: cinema.\n\n5. /gettodayfunctions brings a list of the functions of the movie in a cinema. Use two parameters: cinema and movie.\n\n6. /getdates brings a list of the dates of the movie in a cinema. Use two parameters: cinema and movie.\n\n7. /getfunctionsfordate brings a list of the functions of the movie in a cinema in a specific day. Use three parameters: cinema, movie and date with the format dd/mm.\n\n8. for parameters, replace spaces with underscore(_) and the movie always in mayus"
+    helpMessage = "This bot only works with commands:\n\n1. /getmovies brings a list of the current movies in cinemark.\n\n2. /getcinemas brings a list of the current cinemas in cinemark.\n\n3. /getcinemasformovie brings a list of the current cinemas that display the movie that you're looking for. Use one parameter: movie.\n\n4. /getmoviesforcinema brings a list of the current movies that are in the cinema that you're looking for. Use one parameter: cinema.\n\n5. /gettodayfunctions brings a list of the functions of the movie in a cinema. Use two parameters: cinema and movie.\n\n6. /getdates brings a list of the dates of the movie in a cinema. Use two parameters: cinema and movie.\n\n7. /getfunctionsfordate brings a list of the functions of the movie in a cinema in a specific day. Use three parameters: cinema, movie and date with the format dd/mm.\n\n8. /help brings the list of commands.\n\n9. for parameters, replace spaces with underscore(_) and the movie always in mayus"
     await update.message.reply_text(helpMessage)
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -148,11 +136,11 @@ def main() -> None:
 
     app.add_handler(CommandHandler("getmovies", movies_command))
     app.add_handler(CommandHandler("getcinemas", cinemas_command))
-    app.add_handler(CommandHandler("getcinemasformovie", cinemas_movie_command , pass_args=True))
-    app.add_handler(CommandHandler("getmoviesforcinema", movies_cinema_command, pass_args=True))
-    app.add_handler(CommandHandler("getdates", dates_movie_command, pass_args=True))
-    app.add_handler(CommandHandler("gettodayfunctions", today_functions_command, pass_args=True))
-    app.add_handler(CommandHandler("getfunctionsfordate", functions_date_command, pass_args=True))
+    app.add_handler(CommandHandler("getcinemasformovie", cinemas_movie_command))
+    app.add_handler(CommandHandler("getmoviesforcinema", movies_cinema_command))
+    app.add_handler(CommandHandler("getdates", dates_movie_command))
+    app.add_handler(CommandHandler("gettodayfunctions", today_functions_command))
+    app.add_handler(CommandHandler("getfunctionsfordate", functions_date_command))
     app.add_handler(CommandHandler("help", help_command))
     
     app.add_handler(MessageHandler(filters.TEXT, handle_message))
